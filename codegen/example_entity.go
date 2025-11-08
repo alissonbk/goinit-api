60b13c98025829dca58a59c62426305c264aae0c
alissonbk <alissonbk2010@hotmail.com>
2025-11-08 16:58:47 -0300

    feat: codegen example entity

---
+++ codegen/example_entity.go
@@ -0,0 +1,22 @@
+package codegen
+
+import "fmt"
+
+func GenerateExampleEntity() string {
+	quotationMark := "`"
+	return fmt.Sprintf(`
+	package entity
+
+	import (
+		"time"
+	)
+
+	type BaseEntity struct {
+		ID        uint      %[1]sdb:"id"\%[1]s
+		CreatedAt time.Time %[1]sdb:"created_at"%[1]s
+		UpdatedAt time.Time %[1]sdb:"updated_at"%[1]s
+		DeletedAt time.Time %[1]sdb:"deleted_at"%[1]s
+	}
+
+	`, quotationMark)
+}
